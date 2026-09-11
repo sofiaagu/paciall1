038b9c32dda48e3a8a6ad06d058f886e2089b258
sofiaagu <223770524+sofiaagu@users.noreply.github.com>
2026-09-10 23:16:08 -0500

    .

---
+++ parciall/Assets/Scripts/Desafio2Manager/BotonSerpienteDesafio2.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class BotonSerpienteDesafio2 : MonoBehaviour
+{
+    public Desafio2Manager manager;
+
+    private bool activado = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (activado)
+            return;
+
+        if (other.CompareTag("Serpiente"))
+        {
+            activado = true;
+
+            manager.EstabilizarPuente();
+
+            Debug.Log("🐍 La serpiente presionó el botón");
+        }
+    }
+}
---
+++ parciall/Assets/Scripts/Desafio2Manager/CajaObjetivo2.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CajaObjetivo2 : MonoBehaviour
+{
+    public Desafio2Manager manager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Caja"))
+        {
+            manager.cajaEnPosicion = true;
+
+            Debug.Log("📦 La caja está en la posición del desafío 2");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Caja"))
+        {
+            manager.cajaEnPosicion = false;
+
+            Debug.Log("📦 La caja salió de la posición");
+        }
+    }
+}
